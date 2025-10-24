19df0a506b9fd7ba5a470a111f533080c063e349
linhk123 <dokieulinh582005@gmail.com>
2025-10-24 23:17:20 +0700

    update something

---
+++ src/main/java/com/web_time_system/money/money.java
@@ -0,0 +1,30 @@
+package com.web_time_system.money;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="money",urlPatterns = "/convert")
+public class money extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        float rate = Float.parseFloat(req.getParameter("rate"));
+        float usd = Float.parseFloat(req.getParameter("usd"));
+        float vnd =rate * usd;
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<h1>Rate: " + rate + "</h1>");
+        out.println("<h1>USD: " + usd + "</h1>");
+        out.println("<h1>VND: " + vnd + "</h1>");
+        out.println("</html>");
+    }
+}
